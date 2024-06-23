299b31d6ad15347f11e6db8bd5b2ee7f76bd6bac
silasg <silasg@users.noreply.github.com>
2024-06-23 12:57:58 +0200

    Initial Commit

---
+++ helloworld/helloworldApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloworldApp.swift
+//  helloworld
+//
+//  Created by Silas Graffy on 23.06.24.
+//
+
+import SwiftUI
+
+@main
+struct helloworldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
